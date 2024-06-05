bad1ae61a4563787062295a183ea7b2c8cd0bf36
josé Altamirano <jose@retrypay.com>
2024-06-05 14:05:28 -0500

    add soft delete into models

--- backend/app/Models/Record.php
+++ backend/app/Models/Record.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Record extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'operation_id',
